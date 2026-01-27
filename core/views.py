92fa7cec96572c223ab9a52a9e13060620f68c20
kaloiyap7177-stack <kaloiyap7177@gmail.com>
2026-01-27 14:14:16 +0530

    Day 2: added HTML template and updated view

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to Django Practice")
+    return render(request, 'home.html')
 
 
 # Create your views here.
